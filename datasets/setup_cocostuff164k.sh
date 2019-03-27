cece5e22144fd88d38c47ee1b59a6abf03664199
xuji <xuji@robots.ox.ac.uk>
2019-03-27 01:11:39 +0000

    adding datasets readme

---
+++ datasets/setup_cocostuff164k.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script is the same as the one used by DeepLab Pytorch:
+# https://github.com/kazuto1011/deeplab-pytorch/blob/master/scripts/setup_cocostuff164k.sh
+
+DATASET_DIR=$1
+
+# Download COCO-Stuff 164k (20GB+)
+wget -nc -P $DATASET_DIR http://images.cocodataset.org/zips/train2017.zip
+wget -nc -P $DATASET_DIR http://images.cocodataset.org/zips/val2017.zip
+wget -nc -P $DATASET_DIR http://calvin.inf.ed.ac.uk/wp-content/uploads/data/cocostuffdataset/stuffthingmaps_trainval2017.zip
+
+mkdir -p $DATASET_DIR/images
+mkdir -p $DATASET_DIR/annotations
+unzip -n $DATASET_DIR/train2017.zip -d $DATASET_DIR/images/
+unzip -n $DATASET_DIR/val2017.zip -d $DATASET_DIR/images/
+unzip -n $DATASET_DIR/stuffthingmaps_trainval2017.zip -d $DATASET_DIR/annotations/
+
+echo ===============================================================================================
+echo "Set the path below to \"ROOT:\" in the config/cocostuff164k.yaml:"
+echo -e "\033[32m $DATASET_DIR \033[00m"
+echo ===============================================================================================
